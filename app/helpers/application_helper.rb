4f8142e596f9a88a6db5eebc7d3b4debd2afe776
YC Teoh <ycteoh@YCs-MBA.local>
2023-04-18 22:19:43 +0800

    additional permission for Announcement and initialise UserMgmt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def errmsg_no_permission_to_access
+    return "Sorry but you do not have the permission to access this feature."
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users, :skip => [:registrations], :controllers => { registrations: 'users/registrations', sessions: 'users/sessions' },
              path: '', path_names: { sign_in: 'login', sign_up: 'signup', sign_out: 'logout' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -14,6 +15,8 @@ Rails.application.routes.draw do
   # These routes goes last whenever possible to avoid `new` controllers getting override.
   get "/announcements/:announcement_id", to: "announcements#show"
   get "/announcements/:announcement_id/edit", to: "announcements#edit"
+
+  get "/usermgmt", to: "users#index"
 
   resources :announcements, param: :announcement_id
 
