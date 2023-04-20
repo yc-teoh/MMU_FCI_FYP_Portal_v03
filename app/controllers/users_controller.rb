bb611b81742d5510a4db63678107fe3cbb313e1d
YC Teoh <ycteoh@YCs-MBA.local>
2023-04-20 12:51:28 +0800

    partial UserMgmt search done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
 
     if curr_usr_role == "Manager" || curr_usr_role == "Coordinator"
       @users = User.all
+
+      if params[:search_by_id] && params[:search_by_id] != ""
+        # @users = @users.where("user_id like ?", params[:search_by_id])
+        @users = @users.where('user_id like :search OR user_name like :search', search: "%#{params[:search_by_id]}%")
+      end
+
     end
   end
 end
