c906e78da3e23b4b3113e212fd5fdca8d2e4c5cb
YC Teoh <ycteoh@YCs-MBA.local>
2023-05-23 22:33:57 +0800

    minor front-end adjustment - index page

--- app/controllers/presentation_slots_controller.rb
+++ app/controllers/presentation_slots_controller.rb
@@ -2,7 +2,7 @@ class PresentationSlotsController < ApplicationController
   def index
     curr_usr_role = current_user.user_role    # The role of current user.
 
-    if curr_usr_role == "Manager"
+    if curr_usr_role == "Manager" || curr_usr_role == "Coordinator"
       @pres_slots = PresentationSlot.all.order(presentation_id: :desc)
     end
   end
