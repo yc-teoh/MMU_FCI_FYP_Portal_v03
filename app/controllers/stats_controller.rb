862771ac82f93aa572ed68592d42398e885a9666
YC Teoh <ycteoh@YCs-MBA.local>
2023-07-15 17:17:21 +0800

    Stats - minor fix of approval_status

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -6,30 +6,30 @@ class StatsController < ApplicationController
 
     # BCS-SE
     @projects_bcsse = Project.where(:project_specialisation => "BCS-SE").order(project_id: :asc)
-    @projects_bcsse_active = @projects_bcsse.where(:project_status => "Active")
+    @projects_bcsse_active = @projects_bcsse.where(:project_status => "Active", :project_approval_status => "Confirmed")
 
     # BCS-DS
     @projects_bcsds = Project.where(:project_specialisation => "BCS-DS").order(project_id: :asc)
-    @projects_bcsds_active = @projects_bcsds.where(:project_status => "Active")
+    @projects_bcsds_active = @projects_bcsds.where(:project_status => "Active", :project_approval_status => "Confirmed")
 
     # BCS-GD
     @projects_bcsgd = Project.where(:project_specialisation => "BCS-GD").order(project_id: :asc)
-    @projects_bcsgd_active = @projects_bcsgd.where(:project_status => "Active")
+    @projects_bcsgd_active = @projects_bcsgd.where(:project_status => "Active", :project_approval_status => "Confirmed")
 
     # BCS-AI
     @projects_bcsai = Project.where(:project_specialisation => "BCS-AI").order(project_id: :asc)
-    @projects_bcsai_active = @projects_bcsai.where(:project_status => "Active")
+    @projects_bcsai_active = @projects_bcsai.where(:project_status => "Active", :project_approval_status => "Confirmed")
 
     # BCS-CY
     @projects_bcscy = Project.where(:project_specialisation => "BCS-CY").order(project_id: :asc)
-    @projects_bcscy_active = @projects_bcscy.where(:project_status => "Active")
+    @projects_bcscy_active = @projects_bcscy.where(:project_status => "Active", :project_approval_status => "Confirmed")
 
     # BIT
     @projects_bit = Project.where(:project_specialisation => "BIT").order(project_id: :asc)
-    @projects_bit_active = @projects_bit.where(:project_status => "Active")
+    @projects_bit_active = @projects_bit.where(:project_status => "Active", :project_approval_status => "Confirmed")
 
     # DIP-IT
     @projects_dipit = Project.where(:project_specialisation => "DIP-IT").order(project_id: :asc)
-    @projects_dipit_active = @projects_dipit.where(:project_status => "Active")
+    @projects_dipit_active = @projects_dipit.where(:project_status => "Active", :project_approval_status => "Confirmed")
   end
 end
