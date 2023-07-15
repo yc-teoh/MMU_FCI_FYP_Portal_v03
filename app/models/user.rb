c790f97b2e07830b7503a9868127fd2d957e12b4
YC Teoh <ycteoh@YCs-MBA.local>
2023-07-16 00:02:53 +0800

    supervisor filter list fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   scope :list_student, ->{ where(user_role: "Student") }
   scope :list_coordinator, -> { where(user_role: "Coordinator") }
   scope :list_manager, -> { where(user_role: "Manager") }
-  scope :list_supervisor, -> { where(user_role: "Supervisor") }
+  scope :list_supervisor, -> { where(is_supervisor: "Y") }
   scope :list_moderator, -> { where(user_role: "Moderator") }
 
   validates :user_id, length: { minimum: 6, maximum: 10 }, presence: true, uniqueness: true
